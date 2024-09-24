dc82f151a78257dc2faf373c96092ffe4f9843c8
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2024-09-24 13:04:11 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.kt
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.kt
@@ -4,7 +4,7 @@ class Solution {
         var odd = 0
         var even = 0
         for(i in num_list.indices){
-            if(i % 2 != 0){
+            if(i % 2 == 0){
                 odd+= num_list[i]
             }else{
                 even+= num_list[i]
