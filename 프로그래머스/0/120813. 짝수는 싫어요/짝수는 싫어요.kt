d7684e6fe8db388e7e2ad4dc93155b017b688e2e
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2024-09-24 11:09:18 +0900

    [level 0] Title: 짝수는 싫어요, Time: 11.50 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        for(i in 1..n){
+            if(i % 2 != 0){
+                answer+= i
+            }
+        }
+        return answer
+    }
+}
