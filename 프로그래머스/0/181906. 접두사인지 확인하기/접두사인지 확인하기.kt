64aa3120a31ecf0637570b9d336a9eb3e49f5b9f
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2024-09-24 11:15:19 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 9.47 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String, is_prefix: String): Int {
+        var answer: Int = 0
+        if(my_string.startsWith(is_prefix)) answer++
+        return answer
+    }
+}
