a24f7fb4d34e261d72cc8d7392583f547e62c3e8
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2024-09-24 11:45:50 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 45.92 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.kt
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.kt
@@ -1,10 +1,5 @@
 class Solution {
     fun solution(arr: IntArray, k: Int): IntArray {
-        var answer: IntArray = intArrayOf()
-        if(k % 2 != 0){
-            return arr.map{ it * k }.toIntArray()
-        }else {
-            return arr.map{ it + k }.toIntArray()
-        }
+        return arr.map{ if( k % 2 != 0) it * k else it + k }.toIntArray()
     }
 }
