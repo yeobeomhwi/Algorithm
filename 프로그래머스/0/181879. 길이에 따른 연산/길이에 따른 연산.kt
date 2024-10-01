41a04f435b913acedf93011e699a51998d75373a
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2024-10-01 15:32:51 +0900

    [level 0] Title: 길이에 따른 연산, Time: 20.76 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.kt
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.kt
@@ -1,19 +1,10 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
-        var answer: Int = 1
-        var length = num_list.size
-        num_list.forEach{ num ->
-            if(length > 10){
-                answer += num
-                
-            }else{
-                answer *= num
-            }
+        val length = num_list.size
+        return if (length > 10) {
+            num_list.sum() 
+        } else {
+            num_list.reduce { acc, num -> acc * num } 
         }
-        
-        if(length >10){
-            answer--
-        }
-        return answer
     }
 }
