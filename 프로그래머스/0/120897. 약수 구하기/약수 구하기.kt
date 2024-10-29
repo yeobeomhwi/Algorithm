7e34cd8cb8e77731ac5d8fb4f2679640ccfb79a6
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2024-10-29 09:30:37 +0900

    [level 0] Title: 약수 구하기, Time: 4.50 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.kt
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(n: Int): IntArray {
-        var answer: IntArray = intArrayOf()
-        for(i in 1..n){
-            if(n % i == 0){
-                answer+= i
-            }
-        }
-        return answer
-    }
+    fun solution(n: Int)= (1..n).filter { n % it == 0 }
 }
